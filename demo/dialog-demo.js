4b1f84fbeac1889f07ab01e3ce4ad560a66f936d
web-padawan <serguey.kulikov@gmail.com>
2018-04-03 14:15:27 +0300

    Prepare for the P3 conversion

---
+++ demo/dialog-demo.js
@@ -0,0 +1,14 @@
+/* @polymerMixin */
+DialogDemo = superClass => {
+  return class extends superClass {
+    static get properties() {
+      return {
+      };
+    }
+  };
+};
+
+window.addEventListener('WebComponentsReady', () => {
+  document.body.removeAttribute('unresolved');
+});
+
